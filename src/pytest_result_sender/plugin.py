de9107abd5ee680c7535785a7a483ff1495050e3
username <username@example.com>
2025-04-02 16:59:50 +0800

    使用black、isort、flake8代码质量控制

--- src/pytest_result_sender/plugin.py
+++ src/pytest_result_sender/plugin.py
@@ -1,12 +1,26 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+import pytest
 
+
+data = {
+
+
+}
 
 
 def pytest_configure():
     # 配置加载完毕之后执行，测试用例执行之前执行
+    data['start_time'] = datetime.now()
     print(f"{datetime.now()} pytest开始执行")
 
 
 def pytest_unconfigure():
     # 在配置卸载之后执行，所有测试用例执行后执行
+    data['end_time'] = datetime.now()
     print(f"{datetime.now()} pytest结束执行")
+
+
+    data['duration'] = data['end_time'] - data['start_time']
+    print(data)
+    assert timedelta(seconds=3) >= data['duration'] >= timedelta(seconds=2.5)
+    assert data['total'] == 3
